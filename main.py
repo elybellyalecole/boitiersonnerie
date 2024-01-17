9355e4e9e70ec11eb80928a73a0b26415f5f068a
elybellyalecole <elias.helfand@lifeschooljakarta.com>
2024-01-17 09:41:48 +0700

    CHeck timer works

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 # Importation des librairies
-import time, datetime, calendar # Temps, date, calendrier
+import time, calendar # Temps, date, calendrier
+from datetime import datetime as dt
 import pandas # Gestion des fichiers CSV
+
+def check_time():
+    now = dt.now()
+    current_time = now.strftime("%H:%M")
+    return current_time
 
 class Sonnerie:
     ''' Proprietes: horaires (liste),  '''
     def __init__(self, horaires):
         self.horaires = horaires
 
-    def check_sonnerie(self):
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
+    
+primaire = Sonnerie(['07:45', '08:40', '08:45', '09:40'])
+secondaire = Sonnerie(['07:45', '08:40', '08:45', '09:40'])
+
+while True:
+    if (check_time() in primaire.horaires):
+        print("Sonne")
+        time.sleep(60)
+    else:
+        print("Pas sonne")
+        
