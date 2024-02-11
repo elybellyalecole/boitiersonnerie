d3764f86263154afc4061289a212e758c08fa191
Elias Helfand <helfandelias@gmail.com>
2024-02-11 15:51:52 +0700

    Fonction pour lire CSV, horaires mis en CSV

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 # Importation des librairies
 import time, calendar # Temps, date, calendrier
 import datetime as dt
-import pandas # Gestion des fichiers CSV
+import pandas as pd # Gestion des fichiers CSV
 from pygame import mixer # Gestion du son
 # import RPi.GPIO as GPIO # Gestion du Raspberry Pi
+
+# Lire le fichier CSV
+dataVacances = pd.read_csv("data/vacances.csv")
+dataHoraires = pd.read_csv("data/horaires.csv")
+
+# Variables des vacances + sonneries
+vacances = []
+for i in range(len(dataVacances)):
+    vacances.append((dataVacances['DATEDEBUT'][i], dataVacances['DATEFIN'][i]))
+print(vacances)
+horairePrimaire = dataHoraires['PRIMAIRE'].tolist()
+horaireSecondaire = dataHoraires['SECONDAIRE'].tolist()
+
 
 # Variables vers les ficihers de son
 sonPrimaire = "./sons/primaire.wav"
 sonSecondaire = "./sons/secondaire.wav"
 sonMatinale = "./sons/matinale.wav"
 
-# Variables des sonneries
-horairePrimaire = ["07:45:00.0", "08:30:00.0", "09:15:00.0", "09:45:00.0", "10:15:00.0", "10:45:00.0", "11:00:00.0", "11:45:00.0", "13:00:00.0", "13:45:00.0", "14:30:00.0"]
-horaireSecondaire = ["07:45:30.0", "08:40:00.0", "08:45:00.0", "09:40:00.0", "10:00:00.0", "10:55:00.0", "11:00:30.0", "11:55:00.0", "12:00:00.0", "12:55:00.0", "13:50:00.0", "14:45:00.0", "14:50:00.0", "15:45:00.0", "15:55:00.0", "16:50:00.0", "16:55:00.0", "17:50:00.0"]
 
 # Variables des jours ou la sonnerie ne sonne pas (vacances + jours feries + weekend)
 joursnonsonne = []
@@ -20,6 +30,7 @@ joursnonsonne = []
 # Initialiser la module de mixer de pygame pour le son
 mixer.init()
 
+# Fonctions
 def check_time():
     ''' Fonction qui renvoie le temps actuel en string de format H:M:S '''
     current_time = dt.datetime.now().strftime("%H:%M:%S.%f")[:-5]
@@ -35,7 +46,7 @@ def sonner(nom, son):
     print(f"SONNERIE {nom} EN MARCHE")
     mixer.Channel(0).play(mixer.Sound(son)) # Play le son
 
-def vacances(dateDebut, dateFin):
+def check_vacances(dateDebut, dateFin):
     '''
     str, str -> bool
     Fonction qui prend en entrée la date du debut et de la fin des vacances et renvoie True si le jour d'aujourd'hui se situe entre ces 2 dates
@@ -61,11 +72,11 @@ def vacances(dateDebut, dateFin):
 
     return False
 
-assert vacances('2024-02-05', '2024-02-12') == True
-assert vacances('2024-01-12', '2024-02-12') == True
-assert vacances('2023-01-12', '2024-02-12') == True
-assert vacances('2024-05-12', '2024-06-12') == False
-assert vacances('2024-01-10', '2024-03-10') == True
+assert check_vacances('2024-02-05', '2024-02-12') == True
+assert check_vacances('2024-01-12', '2024-02-12') == True
+assert check_vacances('2023-01-12', '2024-02-12') == True
+assert check_vacances('2024-05-12', '2024-06-12') == False
+assert check_vacances('2024-01-10', '2024-03-10') == True
     
 
 while True:
@@ -74,27 +85,33 @@ while True:
     d = check_date()
     date = dt.date(int(d[0:4]), int(d[5:7]), int(d[8:10]))
 
-    # Si on est pas en weekend
-    if date.weekday() <= 4:
-        # Musique matinale 7:35:00 - 7:45:00
-        if (t=="07:35:00.0"):
-            print("MUSIQUE MATINALE EN MARCHE")
-            mixer.Channel(1).play(mixer.Sound(sonMatinale), -1, 600000) # Play le son sonMatinale avec un loop de -1 (infini) pour 600000ms (10min)
-
-        # Si on est le Vendredi
-        if date.weekday() == 4:
-            # Sonneries primaire/secondaire seulement jusqu'a 11:45 et 12:55 (respectivement)
-            if (t in horairePrimaire[:8]):
-                sonner("PRIMAIRE", sonPrimaire)
-            elif (t in horaireSecondaire[:10]):
-                sonner("SECONDAIRE", sonSecondaire)
-        # Les jours de la semaine
-        else:
-            # Sonneries primaire/secondaire
-            if (t in horairePrimaire):
-                sonner("PRIMAIRE", sonPrimaire)
-            elif (t in horaireSecondaire):
-                sonner("SECONDAIRE", sonSecondaire)
+    # Check si on est en vacances
+    if check_vacances():
+        # Si on est en vacances
+        print("EN VACANCES, PAS DE SONNERIE")
     else:
-        print("Weekend, pas de sonnerie")
+        #Si on est pas en vacances
+        # Check si on est en weekday
+        if date.weekday() <= 4:
+            # Musique matinale 7:35:00 - 7:45:00
+            if (t=="07:35:00.0"):
+                print("MUSIQUE MATINALE EN MARCHE")
+                mixer.Channel(1).play(mixer.Sound(sonMatinale), -1, 600000) # Play le son sonMatinale avec un loop de -1 (infini) pour 600000ms (10min)
+
+            # Check si on est le Vendredi
+            if date.weekday() == 4:
+                # Sonneries primaire/secondaire seulement jusqu'a 11:45 et 12:55 (respectivement)
+                if (t in horairePrimaire[:8]):
+                    sonner("PRIMAIRE", sonPrimaire)
+                elif (t in horaireSecondaire[:10]):
+                    sonner("SECONDAIRE", sonSecondaire)
+            else:
+                # Sonneries primaire/secondaire
+                if (t in horairePrimaire):
+                    sonner("PRIMAIRE", sonPrimaire)
+                elif (t in horaireSecondaire):
+                    sonner("SECONDAIRE", sonSecondaire)
+        # Si on est en weekend
+        else:
+            print("EN WEEKEND, PAS DE SONNERIE")
         
